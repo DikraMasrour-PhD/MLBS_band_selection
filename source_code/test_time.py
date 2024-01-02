c4bfbe1e8f595ff28890860c81ec1d7b34576f8c
Dikra <dirkra.masrour@uir.ac.ma>
2024-01-02 13:06:56 +0100

    edit test code

--- source_code/test_time.py
+++ source_code/test_time.py
@@ -3,24 +3,24 @@ import tensorflow as tf
 import time
 
 # Defining constants
-DATASET = "IP"
+DATASET_NAME = "IP"
 
 # Model and dataset addresses
-MODEL_ADDRESS = "hbs_mlbs_"+DATASET
-DATASE_ADDRESST = ["hbs_mlbs_"+DATASET+"\\image_train_"+DATASET+".npy",
-                   "hbs_mlbs_"+DATASET+"\\label_train_"+DATASET+".npy",
-                   "hbs_mlbs_"+DATASET+"\\image_test_"+DATASET+".npy",
-                   "hbs_mlbs_"+DATASET+"\\label_test_"+DATASET+".npy"]
+MODEL_ADDRESS = "hbs_mlbs_"+DATASET_NAME
+DATASET_ADDRESS = ["hbs_mlbs_"+DATASET_NAME+"\\image_train_"+DATASET_NAME+".npy",
+                   "hbs_mlbs_"+DATASET_NAME+"\\label_train_"+DATASET_NAME+".npy",
+                   "hbs_mlbs_"+DATASET_NAME+"\\image_test_"+DATASET_NAME+".npy",
+                   "hbs_mlbs_"+DATASET_NAME+"\\label_test_"+DATASET_NAME+".npy"]
 
 # Loading the training dataset.
 model = tf.keras.models.load_model(MODEL_ADDRESS)
-image_train = np.load(DATASE_ADDRESST[0])
-label_train = np.load(DATASE_ADDRESST[1])
+image_train = np.load(DATASET_ADDRESS[0])
+label_train = np.load(DATASET_ADDRESS[1])
 label_train_int = np.argmax(label_train, axis=1)
 
 start_time = time.time()
-#model.predict(image_train[0][np.newaxis])
-model.predict([image_train[0][np.newaxis], image_train[0][np.newaxis]])
+model.predict(image_train)
+# model.predict([image_train[0][np.newaxis], image_train[0][np.newaxis]])
 end_time = time.time()
 
 # Calculate elapsed time
